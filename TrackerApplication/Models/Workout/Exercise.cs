caec1824a72da8f9c67a22dd8f9b3c582fd3dc71
alicatron <alison.ronayne@gmail.com>
2017-06-01 14:40:08 +0100

    Add project files.

---
+++ TrackerApplication/Models/Workout/Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackerApplication.Models.Workout
+{
+    public enum BodyPart
+    {
+        Abs, Arms, Back, Chest, Legs, Shoulders
+    }
+
+    public class Exercise
+    {
+        public int ExerciseID { get; set; }
+        public String ExerciseName { get; set; }
+
+        public BodyPart bodyPart { get; set; }
+
+        public int WorkoutID { get; set; }
+        public virtual ICollection<Workout> Workout { get; set; }
+
+    }
+}
---
+++ TrackerApplication/Models/Workout/Workout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrackerApplication.Models.Workout
+{
+    public class Workout
+    {
+        public int WorkoutID { get; set; }
+
+        public double WeightLifted { get; set; }  //kilograms
+
+        public int Repetition { get; set; } //The amount of lifts done
+
+        public int Set { get; set; } // The amount of times a group of repitions are done
+
+        [Display(Name = "Workout Date")]
+        [DataType(DataType.Date)]
+        public DateTime WorkoutDate { get; set; }
+
+        public virtual ICollection<Exercise> Exercises { get; set; }
+
+    }
+}
